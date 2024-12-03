03266f42ceef5674d70947c7b8c01a132832cb84
Thibault Taillandier <thibault.taillandier@openclassrooms.com>
2024-12-03 22:55:00 +0100

    fix(package): Fix Uri namespace

--- src/Schema/TypeFormats/StringURI.php
+++ src/Schema/TypeFormats/StringURI.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace OpenClassrooms\OpenAPIValidation\Schema\TypeFormats;
 
-use OpenClassrooms\Uri\Exceptions\SyntaxError;
-use OpenClassrooms\Uri\UriString;
+use League\Uri\Exceptions\SyntaxError;
+use League\Uri\UriString;
 
 class StringURI
 {
     public function __invoke(string $value): bool
     {
         try {
-            // namespace 'OpenClassrooms\Uri' is provided by multiple packages, but PHPStan does not support merging them
+            // namespace 'League\Uri' is provided by multiple packages, but PHPStan does not support merging them
             // @phpstan-ignore-next-line
             UriString::parse($value);
 
